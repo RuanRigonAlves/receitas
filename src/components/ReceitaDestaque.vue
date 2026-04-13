8ad45a16ca24a87bac03d4791aa63b178eea9002
RuanRigonAlves <ruanrigon2k@gmail.com>
2026-04-13 16:42:11 -0300

    receita destaque

--- src/components/ReceitaDestaque.vue
+++ src/components/ReceitaDestaque.vue
@@ -1,21 +1,34 @@
 <template>
-  <v-container>
-    <v-row max-height="300">
-      <v-col cols="8" class="d-flex flex-column justify-space-evenly">
-        <div>{{ props.receita.strMeal }}</div>
-        <div>{{ props.receita.strInstructions }}</div>
-        <div>ir para receita</div>
-      </v-col>
-      <v-col cols="4">
-        <v-img :src="props.receita.strMealThumb"></v-img>
-      </v-col>
-    </v-row>
-  </v-container>
-  <pre>
-    {{ props.receita }}
-  </pre>
+  <v-row :style="mobile ? 'flex-direction: column-reverse;' : ''">
+    <v-col
+      sm="8"
+      cols="12"
+      class="d-flex flex-column justify-space-evenly pt-2"
+    >
+      <v-card-title>{{ props.receita.strMeal }}</v-card-title>
+      <p class="pl-3">{{ props.receita.strInstructions }}</p>
+      <div class="pl-3 pb-3">
+        <v-btn class="px-8" :to="`/receita/${props.receita.idMeal}`" rounded>
+          Conhecer mais sobre a receita
+        </v-btn>
+      </div>
+    </v-col>
+
+    <v-col cols="12" sm="4">
+      <v-img
+        :src="props.receita.strMealThumb"
+        height="400px"
+        width="100%"
+        cover
+      ></v-img>
+    </v-col>
+  </v-row>
 </template>
 
 <script setup>
+import { useDisplay } from "vuetify";
+
+const { mobile } = useDisplay();
+
 const props = defineProps(["receita"]);
 </script>
